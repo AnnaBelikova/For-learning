615f8a7cf3a191d46c8b888238b75472b62a6d3f
daragunchik <daragunchik@gmail.com>
2019-04-30 23:24:47 +0300

    "Return false" added.

--- JavaScript/8_exercise.js
+++ JavaScript/8_exercise.js
@@ -1,48 +1,44 @@
 function deepEqual(a,b){
-    let result=true;
     if(typeof(a)!='object' && typeof(b)!='object'){
-        result = ( a === b ) ? true : false;
+        /*result = ( a === b ) ? true : false;*/
+        if(a!=b){
+            return false;
+        }
     }
-    
-    else if(null!=undefined && a === null && b === null ){
-        result = true;
+    else if(a === null && b!=null || b === null && a!=null){
+        return false;
     }
-
     else if (a===null && b===undefined || a===undefined && b===null){
-        result = false;
+        return false;
     }
-    
-    else if(a == null && b!=null || b == null && a!=null){
-        result=false;
-    }
-    else if(typeof(a)=='object' && typeof(b)=='object'){
+    else if(typeof(a)==='object' && typeof(b)==='object' && a!=null && b!=null){
         if(Object.keys(a).length != Object.keys(b).length){
-            result=false;
+            return false;
         }
         else{
             for(let prop in a ){
                 if(!deepEqual(a[prop],b[prop])) {
-                    result = false;
-                    break;
+                    return false;
                 }
-                result=true;
             }
         }
     }
-    return result;
+    return true;
 }
 
 
 
-/*let a= new Object();
+let a= new Object();
 a.name='Derek';
 a.age=67;
 a.education='elementary';
 let b= new Object();
 b.name='Derek';
-b.age=67;
-b.education='elementary';*/
-let a = null;
-let b = undefined;
+b.age=64;
+b.education='elementary';
+/*let a = null;
+let b = undefined;*/
+/*a=1;
+b=1;*/
 
 console.log(deepEqual(a,b));
