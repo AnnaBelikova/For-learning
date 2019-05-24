7f1d943af05979e46ed365ce617b5df417aaf4c3
daragunchik <daragunchik@gmail.com>
2019-05-25 01:15:54 +0300

    Added burger-menu. Addaptive project.

--- UIXREX/uix/uix.js
+++ UIXREX/uix/uix.js
@@ -241,3 +241,22 @@ changePoint('.points','.comments-point');
         }
       };
 
+
+
+      
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+    });
+    
+    $('.menu__links-item').on('click', function() {
+
+      $(this).closest('.menu')
+        .removeClass('menu_state_open');
+    });
+  });
+})(jQuery);
+
