b0fa3fa2aa266307a4f25ce3627e81cd26fc872e
daragunchik <daragunchik@gmail.com>
2019-06-17 01:08:27 +0300

    Trying to optimize my cod (not successfully).

--- MyJSTasks/TicTacToe/TicTacToe.js
+++ MyJSTasks/TicTacToe/TicTacToe.js
@@ -1,22 +1,8 @@
-let a=0;
-let b=0;
+let playerSign=0;
+let compSign=0;
+
 let row=0;
 let col=0;
-
-//Порядок хода
-function randomPlayer() {
-    let rand= Math.random()*10;
-    if(rand>5){
-        a=2;
-        b=1;
-        alert('Computer starts!');
-        computerStep();
-    }else{
-        a=1;
-        b=2;
-        alert('Player starts!');
-    }
-}
 
 let arr=[
     [0,0,0],
@@ -24,14 +10,32 @@ let arr=[
     [0,0,0]
 ];
 
-//Check is the element 0 or not. Change to 1 or 2.
+let winner='';
+
+//Game logic.
+//Define the order of the course.
+function randomPlayer() {
+    let rand= Math.random()*10;
+    if(rand>5){
+        playerSign=2;
+        compSign=1;
+        alert('Computer starts!');
+        computerStep();
+    }else{
+        playerSign=1;
+        compSign=2;
+        alert('Player starts!');
+    }
+}
+
+//Auxiliary function for checking is the element 0 or not. If true - change to 1 or 2.
 function fillTheField(o,t,sign){
     if (arr[o][t]===0){
         arr[o][t]=sign;
     }
 }
 
-//ИИ пробуем задать условия.
+//Auxiliary function for computer's course(random move).
 function makeAStep(){
     let z=Math.floor(Math.random()*3);
     let s=Math.floor(Math.random()*3);
@@ -39,54 +43,22 @@ function makeAStep(){
         z=Math.floor(Math.random()*3);
         s=Math.floor(Math.random()*3);
         }
-    fillTheField(z,s,b);
+    fillTheField(z,s,compSign);
     row=z;
     col=s;
 };
 
-function computerStep(){
-    let i;
-    let j;
-    if(oneStepToWin(b)!==false){
-        i=row;
-        j=col;
-        fillTheField(row,col,b);
-    }else if(oneStepToWin(a)!==false){
-        i=row;
-        j=col;
-        fillTheField(row,col,b);
-    }else if(arr[1][1]===0){
-        i=1;
-        j=1;
-        fillTheField(1,1,b);
-    }else{
-        makeAStep();
-        i=row;
-        j=col;
-    }
-addTheSign(i,j);
-checkTheWin();
-return;
-};
-
-    /*let i=Math.floor(Math.random()*3);
-    let j=Math.floor(Math.random()*3);
-    while (arr[i][j]!==0){
-        i=Math.floor(Math.random()*3);
-        j=Math.floor(Math.random()*3);
-    }*/
-    //fillTheField(i,j,b);
-
+//Auxiliary function for cleaning the field.
 function clearTheField(){
     for(let i=0;i<arr.length;i++){
         for(let j=0;j<arr[i].length;j++){
             arr[i][j]=0;
-            arrVis[i][j].textContent="";;
+            arrVis[i][j].textContent="";
         }
     }
 };
 
-//Check the condition of victory.
+//Check the condition of victory(one step to victory).
 function oneStepToWin(mean){
     for(let k=0;k<arr.length;k++){
         for(let t=0;t<arr[k].length;t++){
@@ -136,46 +108,60 @@ function oneStepToWin(mean){
 };
 
 
-
-function checkTheWin(){
-    if(arr[0][0]==1 && arr[0][1]==1 && arr[0][2]==1 ||
-        arr[1][0]==1 && arr[1][1]==1 && arr[1][2]==1 ||
-        arr[2][0]==1 && arr[2][1]==1 && arr[2][2]==1 ||
-        arr[0][0]==1 && arr[1][0]==1 && arr[2][0]==1 ||
-        arr[0][1]==1 && arr[1][1]==1 && arr[2][1]==1 ||
-        arr[0][2]==1 && arr[1][2]==1 && arr[2][2]==1 ||
-        arr[0][0]==1 && arr[1][1]==1 && arr[2][2]==1 ||
-        arr[0][2]==1 && arr[1][1]==1 && arr[2][0]==1)
-        {
-            alert('1 player WIN!');
-            clearTheField();
-    }else if(arr[0][0]==2 && arr[0][1]==2 && arr[0][2]==2 ||
-            arr[1][0]==2 && arr[1][1]==2 && arr[1][2]==2 ||
-            arr[2][0]==2 && arr[2][1]==2 && arr[2][2]==2 ||
-            arr[0][0]==2 && arr[1][0]==2 && arr[2][0]==2 ||
-            arr[0][1]==2 && arr[1][1]==2 && arr[2][1]==2 ||
-            arr[0][2]==2 && arr[1][2]==2 && arr[2][2]==2 ||
-            arr[0][0]==2 && arr[1][1]==2 && arr[2][2]==2 ||
-            arr[0][2]==2 && arr[1][1]==2 && arr[2][0]==2)
-            {
-                alert('2 player WIN!');
-                clearTheField();
-        }else{
-            for(let k=0;k<arr.length;k++){
-                for(let t=0;t<arr[k].length;t++){
-                    if(arr[k][t]===0){
-                        return;
-                    }
-                }
+//Check the condition of victory.
+function checkTheWin(sign){
+    for(let i=0;i<arr.length;i++){
+        let temp=true;
+        for(let j=0;j<arr[i].length;j++){
+            temp=temp&&(arr[i][j]===sign);
+        }
+        if(temp){
+            winner=sign;
+            return true;
         }
     }
-        alert('NOBODY WIN!');
-        clearTheField();
-};
+    for(let j=0;j<arr.length;j++){
+        let temp=true;
+        for(let i=0;i<arr.length;i++){
+            temp=temp&&(arr[i][j]===sign);
+        }
+        if(temp){
+            winner=sign;
+            return true;
+        }
+    }
+    let temp=true;
+    for(let i=0;i<arr.length;i++){
+        temp=temp&&(arr[i][i]===sign)
+    }
+    if(temp){
+        winner=sign;
+        return true;
+    }
+    temp=true;
+    for(let i=0;i<arr.length;i++){
+        temp=temp&&(arr[i][arr.length-i-1]===sign)
+    }
+    if(temp){
+        winner=sign;
+        return true;
+    }
+    for(let k=0;k<arr.length;k++){
+        for(let t=0;t<arr[k].length;t++){
+            if(arr[k][t]===0){
+                return false;
+            }
+        }
+    }
+
+    winner='NOBODY';
+    return true;
+}
 
 
 
-//Visualization
+
+//Visualization.
 
 let arrItems=[...document.getElementsByClassName('item')];
 let arrRow1=arrItems.slice(0, 3);
@@ -192,36 +178,59 @@ function addTheSign(i,j){
     }
 }
 
-/*function showTheField(){
-    let arrayRow=document.getElementsByClassName('arrayrow');
-    arrayRow[0].textContent=arr[0];
-    arrayRow[1].textContent=arr[1];
-    arrayRow[2].textContent=arr[2];
-};*/
-
-/*for (i=0;i<arrVis.length;i++){
-    for(j=0;j<arrVis[i].length;j++){
-            arrVis[i][j].addEventListener("click", fillTheField(i,j,1));
+function theWinner(){
+    if(winner==compSign){
+        alert('Computer WIN! Press "BEGIN!" to try again.');
+    }else if(winner==playerSign){
+        alert('YOU WIN! Press "BEGIN!" to try again.');
+    }else if(winner=='NOBODY'){
+        alert(winner+' WIN! Press "BEGIN!" to try again.')
+    }else{
+        return;
     }
 }
-*/
 
-/*fillTheField(0,0,1);
-showTheField();
-checkTheWin();  
+//Computer's turn.
+function computerStep(){
+    let i;
+    let j;
+    if(oneStepToWin(compSign)!==false){
+        i=row;
+        j=col;
+        fillTheField(row,col,compSign);
+    }else if(oneStepToWin(playerSign)!==false){
+        i=row;
+        j=col;
+        fillTheField(row,col,compSign);
+    }else if(arr[1][1]===0){
+        i=1;
+        j=1;
+        fillTheField(1,1,compSign);
+    }else{
+        makeAStep();
+        i=row;
+        j=col;
+    }
+addTheSign(i,j);
+if(checkTheWin(compSign)){
+theWinner();
+}else{
+return;
+}
+};
 
-fillTheField(0,1,2);
-showTheField();
-checkTheWin();  
-
-fillTheField(1,1,1);
-showTheField();
-checkTheWin();  
-
-fillTheField(1,2,2);
-showTheField();
-checkTheWin();  
-
-fillTheField(2,2,1);
-showTheField();
-checkTheWin();  */
+//Player's turn.
+for(let i=0;i<arrVis.length;i++){
+    for(let j=0;j<arrVis[i].length;j++){
+        arrVis[i][j].addEventListener('click',function visualization(){
+            fillTheField(i,j,playerSign);
+            addTheSign(i,j,playerSign);
+            if(!checkTheWin(playerSign)){
+                computerStep();
+            }else{
+                theWinner();
+                return;
+            }
+    });
+}
+}
